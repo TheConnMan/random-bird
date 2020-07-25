870d206389b22fd61ba0752133083bd821bbbc42
TheConnMan <bcconn2112@gmail.com>
2020-07-25 19:05:40 -0400

    Remove images and link directly to ebird

--- api/[locale].js
+++ api/[locale].js
@@ -5,23 +5,17 @@ const RSS = require('rss');
 const MS_IN_DAY = 24 * 60 * 60 * 1000;
 
 module.exports = (req, res) => {
-  request(`http://ebird.org/ws1.1/ref/taxa/ebird?fmt=json&locale=${req.query.locale}`, {
+  request(`http://ebird.org/ws1.1/ref/taxa/ebird?cat=species&fmt=json&locale=${req.query.locale}`, {
     json: true
   }).then((birds) => {
     const today = Math.floor(new Date().getTime() / MS_IN_DAY) * MS_IN_DAY;
     return Promise.all([...Array(30).keys()].map((i) => {
       return today - i * MS_IN_DAY;
     }).map((day) => {
-      const bird = birds[Math.floor(birds.length * seedrandom(day)())];
-      return request(`https://ebird.org/media/catalog.json?searchField=species&q=${bird.comName}&taxonCode=${bird.speciesCode}&_mediaType=on&mediaType=p&view=List&sort=rating_rank_desc&_req=on&count=5`, {
-        json: true
-      }).then((images) => {
-        return {
-          date: new Date(day),
-          bird,
-          images: images.results.content
-        };
-      });
+      return {
+        date: new Date(day),
+        bird: birds[Math.floor(birds.length * seedrandom(day)())]
+      };
     }));
   }).then((birds) => {
     res.setHeader('Content-Type', 'text/xml');
@@ -44,17 +38,9 @@ function formatRSS(birds) {
     feed.item({
       title: `${item.bird.comName} (${item.bird.sciName})`,
       url: `https://ebird.org/species/${item.bird.speciesCode}`,
+      link: `https://ebird.org/species/${item.bird.speciesCode}`,
       guid: `https://ebird.org/species/${item.bird.speciesCode}`,
-      date: item.date,
-      custom_elements: item.images.length === 0 ? [] : [{
-        'media:content': {
-          _attr: {
-            url: item.images[0].largeUrl
-          }
-        }
-      }, {
-        'media:credit': item.images[0].userDisplayName
-      }]
+      date: item.date
     });
   });
   return feed.xml();
