306e63f5a14218a5d5c80d8adc7e91cb0cd49da0
TheConnMan <bcconn2112@gmail.com>
2020-07-25 18:18:32 -0400

    Add bird images

--- index.js
+++ index.js
@@ -17,17 +17,25 @@ app.get('/:locale.atom', function (req, res) {
     json: true
   }).then((birds) => {
     const today = Math.floor(new Date().getTime() / MS_IN_DAY) * MS_IN_DAY;
-    const days = [...Array(30).keys()].map((i) => {
+    return Promise.all([...Array(30).keys()].map((i) => {
       return today - i * MS_IN_DAY;
     }).map((day) => {
-      return {
-        date: new Date(day),
-        bird: birds[Math.floor(birds.length * seedrandom(day)())]
-      };
-    });
+      const bird = birds[Math.floor(birds.length * seedrandom(day)())];
+      return request(`https://ebird.org/media/catalog.json?searchField=species&q=${bird.comName}&taxonCode=${bird.speciesCode}&_mediaType=on&mediaType=p&view=List&sort=rating_rank_desc&_req=on&count=5`, {
+        json: true
+      }).then((images) => {
+        return {
+          date: new Date(day),
+          bird,
+          images: images.results.content
+        };
+      });
+    }));
+  }).then((birds) => {
     res.set('Content-Type', 'text/xml');
-    res.send(formatRSS(days));
-  }).catch(e => {
+    res.send(formatRSS(birds));
+  })
+  .catch(e => {
     console.log(e);
     res.sendStatus(500);
   });
@@ -39,14 +47,26 @@ app.listen(3000, function () {
 
 function formatRSS(birds) {
   var feed = new RSS({
-    title: `Birds`
+    title: `Random Bird Images - Daily`,
+    custom_namespaces: {
+      'media': 'http://search.yahoo.com/mrss/'
+    },
   });
   birds.forEach((item) => {
     feed.item({
       title: `${item.bird.comName} (${item.bird.sciName})`,
       url: `https://ebird.org/species/${item.bird.speciesCode}`,
       guid: `https://ebird.org/species/${item.bird.speciesCode}`,
-      date: item.date
+      date: item.date,
+      custom_elements: item.images.length === 0 ? [] : [{
+        'media:content': {
+          _attr: {
+            url: item.images[0].largeUrl
+          }
+        }
+      }, {
+        'media:credit': item.images[0].userDisplayName
+      }]
     });
   });
   return feed.xml();
